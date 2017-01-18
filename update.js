bd426b0c316d809edb39cc2a9a1a38f1aa0ef839
Jakob Unnebäck <jakob@folkdatorn.se>
2017-01-18 22:59:20 +0100

    Extensive mobile support
    Extensive mobile support with collapsable nav bar.

--- update.js
+++ update.js
@@ -3,3 +3,12 @@ function updatevideo (url, title, desc) {
   document.getElementById('titleText').innerHTML = title
   document.getElementById('descText').innerHTML = desc
 }
+var nav = true
+function navbar () {
+  if (nav) {
+    document.getElementById('navcontainer').style.height = 'auto'
+  } else {
+    document.getElementById('navcontainer').style.height = '0'
+  }
+  nav =! nav
+}
