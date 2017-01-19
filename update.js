fc7be9bef27ff6873add575d78692e8be7cb7787
Jakob Unnebäck <jakob@folkdatorn.se>
2017-01-19 22:51:22 +0100

    Dynamic video scrolling
    The site now scrolls so that the video player is always centered.

--- update.js
+++ update.js
@@ -2,6 +2,14 @@ function updatevideo (url, title, desc) {
   document.getElementById('videoplayer').setAttribute('src', 'https://www.youtube.com/embed/' + url)
   document.getElementById('titleText').innerHTML = title
   document.getElementById('descText').innerHTML = desc
+  var windowh = window.innerHeight
+  var videoh = window.innerWidth * 0.36
+  var scroll = 321 - (windowh - videoh) / 2
+  if (window.innerWidth > 700) {
+    window.scrollTo(0, scroll)
+  } else {
+    window.scrollTo(0, 0)
+  }
 }
 var nav = true
 function navbar () {
